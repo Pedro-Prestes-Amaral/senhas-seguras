ffecf28f4b231f4a6138726e80d063633f287609
Pedro-Prestes-Amaral <137920119+Pedro-Prestes-Amaral@users.noreply.github.com>
2025-06-16 16:47:51 +0000

    6

--- script.js
+++ script.js
@@ -1,19 +1,28 @@
 function gerarSenha() {
+  const usarMaiusculas = document.getElementById('maiusculas').checked;
+  const usarMinusculas = document.getElementById('minusculas').checked;
+  const usarNumeros = document.getElementById('numeros').checked;
+
+  let caracteres = '';
   const letrasMaiusculas = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
   const letrasMinusculas = 'abcdefghijklmnopqrstuvwxyz';
   const numeros = '0123456789';
-  const todosCaracteres = letrasMaiusculas + letrasMinusculas + numeros;
 
-  let senha = '';
+  if (usarMaiusculas) caracteres += letrasMaiusculas;
+  if (usarMinusculas) caracteres += letrasMinusculas;
+  if (usarNumeros) caracteres += numeros;
+
+  if (caracteres.length === 0) {
+    document.getElementById('senha').value = 'Selecione ao menos 1 opção!';
+    return;
+  }
+
   const tamanhoSenha = 12;
-
+  let senha = '';
   for (let i = 0; i < tamanhoSenha; i++) {
-    const randomIndex = Math.floor(Math.random() * todosCaracteres.length);
-    senha += todosCaracteres[randomIndex];
+    const index = Math.floor(Math.random() * caracteres.length);
+    senha += caracteres[index];
   }
 
   document.getElementById('senha').value = senha;
 }
-
-// Gera uma senha ao abrir a página
-gerarSenha();
